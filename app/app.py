bbdc2fea1339fef38977feb24509e0412773363a
yattinda <yattinda@gmail.com>
2023-07-04 23:57:26 +0900

    Exclude non PDF

--- app/app.py
+++ app/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request
-from app.func.pdf_to_txt import to_txt
+from flask import Flask, render_template, request, flash
+from app.func.pdf_to_txt import to_txt, allowed_file
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 
 @app.route("/")
@@ -13,8 +15,16 @@ def index():
 def upload_file():
     if request.method == 'POST':
         file = request.files['file']
-        print(to_txt(file))
-        return render_template('show_result.html', result_txt=to_txt(file))
+        if 'file' not in request.files:
+            flash('File not found')
+            return render_template('index.html')
+        elif not allowed_file(file.filename):
+            flash("You can ONLY upload PDF")
+            return render_template('index.html')
+        if file:
+            print(to_txt(file))
+            return render_template('show_result.html', result_txt=to_txt(file))
+
     else:
         return render_template('index.html')
 
